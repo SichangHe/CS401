25d1dbd9a46e72d183a49e3ee02d661db4e6a97e
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2024-02-10 16:50:08 +0800

    rid broken test

--- HW2/rest_server/src/lib.rs
+++ HW2/rest_server/src/lib.rs
@@ -25,7 +25,6 @@ use tokio_util::sync::CancellationToken;
 use tracing::{debug, error, info, instrument, warn};
 
 use actor::{Actor, Ref};
-// use serve::RecommendationResponse;
 
 pub mod actor;
 mod read_rules;
@@ -37,36 +36,13 @@ const FIVE_SECONDS: Duration = Duration::from_secs(5);
 #[main]
 #[instrument(skip(data_dir), fields(data_dir = ?data_dir.as_ref()))]
 pub async fn run(data_dir: impl AsRef<Path>, port: &str) -> Result<()> {
-    let rule_query_server = RuleServer::new(data_dir.as_ref().into());
-    let (query_server_handle, mut query_server_ref) = rule_query_server.spawn();
+    let rule_server = RuleServer::new(data_dir.as_ref().into());
+    let (rule_server_handle, mut rule_server_ref) = rule_server.spawn();
 
-    // _testing(query_sender.clone()).await?;
-    serve::serve(port, query_server_ref.clone()).await?;
+    serve::serve(port, rule_server_ref.clone()).await?;
 
-    query_server_ref.cancel();
-    query_server_handle.await??;
-
-    Ok(())
-}
-
-/*
-async fn _testing(query_sender: Sender<QueryServerMsg>) -> Result<()> {
-    let (response_sender, mut response_receiver) = channel(1);
-    let mock_query = vec![
-        "Ride Wit Me".into(),
-        "Bottle It Up - Acoustic Mixtape".into(),
-        "DNA.".into(),
-    ];
-    let query = QueryServerMsg::Query(mock_query.clone(), response_sender.clone());
-    for _ in 0..20 {
-        query_sender.send(query.clone()).await?;
-        let (playlist_ids, model_date) = response_receiver.recv().await.unwrap();
-        let response = RecommendationResponse::new(playlist_ids, model_date.to_string());
-        let response = serde_json::to_string(&response)?;
-        warn!(response);
-        sleep(FIVE_SECONDS).await;
-    }
+    rule_server_ref.cancel();
+    rule_server_handle.await??;
 
     Ok(())
 }
-*/
