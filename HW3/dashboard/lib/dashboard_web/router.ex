6325ae72dedf23628c652d30b5f145ec1154e4f2
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2024-02-20 21:22:52 +0800

    run dashboard at /

--- HW3/dashboard/lib/dashboard_web/router.ex
+++ HW3/dashboard/lib/dashboard_web/router.ex
@@ -1,6 +1,6 @@
 defmodule DashboardWeb.Router do
   use DashboardWeb, :router
-  import Phoenix.LiveView.Router
+  import Phoenix.LiveDashboard.Router
 
   pipeline :browser do
     plug :accepts, ["html"]
@@ -15,36 +15,11 @@ defmodule DashboardWeb.Router do
     plug :accepts, ["json"]
   end
 
-  # scope "/", DashboardWeb do
-  #   pipe_through :browser
-  #
-  #   live "/", Live
-  # end
+  scope "/" do
+    pipe_through :browser
 
-  # Other scopes may use custom stacks.
-  # scope "/api", DashboardWeb do
-  #   pipe_through :api
-  # end
-
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
-
-    scope "/" do
-      pipe_through :browser
-
-      live_dashboard "/dashboard",
-        metrics: DashboardWeb.Telemetry,
-        additional_pages: [
-          monitoring: DashboardWeb.Live
-        ]
-    end
+    live_dashboard "/",
+      metrics: DashboardWeb.Telemetry
   end
 
   # Enables the Swoosh mailbox preview in development.
