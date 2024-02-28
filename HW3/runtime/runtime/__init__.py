c2fcce8a7756b70663a1c4aec220012c67851a51
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2024-02-28 16:02:00 +0800

    fix runtime not getting through nested module name

--- HW3/runtime/runtime/__init__.py
+++ HW3/runtime/runtime/__init__.py
@@ -3,10 +3,9 @@ import os
 import sys
 from dataclasses import dataclass, field
 from datetime import datetime
-from importlib.machinery import ModuleSpec
+from importlib import import_module
 from importlib.util import module_from_spec, spec_from_file_location
 from types import ModuleType
-from zipimport import zipimporter
 
 logging.basicConfig(format="%(asctime)s [%(levelname)s] %(message)s")
 logger = logging.getLogger(__name__)
@@ -32,9 +31,12 @@ class Context:
     """Dictionary to persist data between executions."""
 
 
-def _import_spec(spec: ModuleSpec | None, module_name: str) -> ModuleType | None:
+def import_file(file_path: str, module_name: str) -> ModuleType | None:
+    """Import a Python file `file_path` as a module named `module_name`."""
     # From <https://docs.python.org/3/library/importlib.html#importing-a-source-file-directly>.
-    if spec is not None and (loader := spec.loader):
+    if (spec := spec_from_file_location(module_name, file_path)) and (
+        loader := spec.loader
+    ):
         module = module_from_spec(spec)
         sys.modules[module_name] = module
         loader.exec_module(module)
@@ -43,14 +45,7 @@ def _import_spec(spec: ModuleSpec | None, module_name: str) -> ModuleType | None
         return None
 
 
-def import_file(file_path: str, module_name: str) -> ModuleType | None:
-    """Import a Python file `file_path` as a module named `module_name`."""
-    spec = spec_from_file_location(module_name, file_path)
-    return _import_spec(spec, module_name)
-
-
 def import_zipped_module(zip_path: str, module_name: str) -> ModuleType | None:
     """Import a Python module `module_name` from the ZIP file at `zip_path`."""
-    importer = zipimporter(zip_path)
-    spec = importer.find_spec(module_name)
-    return _import_spec(spec, module_name)
+    sys.path.insert(0, zip_path)
+    return import_module(module_name)
